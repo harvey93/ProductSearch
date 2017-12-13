cc73436dcdcdc53c01fb0f9e2155174ebd496c58
Hrant Mirijanyan <harvey.mirijanyan@gmail.com>
2017-12-13 09:07:57 -0800

    change port

--- app/controllers/api/product_search_controller.rb
+++ app/controllers/api/product_search_controller.rb
@@ -1,16 +1,30 @@
 class Api::ProductSearchController < ApplicationController
 
   def index
-    #@items = Sem3SearchService.new(params).execute
     res = SearchWord.where(name: params[:search])
     if res.empty?
       puts "Save to db and return"
-      @items = []
+      @items = Sem3SearchService.new(params).execute
+      word = SearchWord.new({name: params[:search]})
+      if word.save!
+        @items.each do |item|
+          name = item["name"]
+          price = item["price"]
+          retailer_name = item['sitedetails'].first['name']
+          retailer_url = item['sitedetails'].first['url']
+          search_result = SearchResult.new({name: name, price: price, retailer_name: retailer_name, retailer_url: retailer_url, search_word_id: word.id})
+          if !search_result.save!
+            render json: ["Unable to save word to cache"], status: 500
+          end
+        end
+      else
+        render json: ["Unable to save word to cache"], status: 500
+      end
     else
       word = res[0]
       puts "Get from cache"
-      @items = word.search_results
     end
+    @items = word.search_results
   end
 
 end
